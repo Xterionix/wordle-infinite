7bf632f61d8a13e44fb88c27506fbaa415914454
Xterionix <xterionyt@gmail.com>
2026-05-27 17:01:11 -0500

    - Add support for react dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
+if (import.meta.env.DEV) {
+  const script = document.createElement('script')
+  script.src = 'http://localhost:8097'
+  document.head.appendChild(script)
+}
+
 root.render(
   <React.StrictMode>
     <App />
