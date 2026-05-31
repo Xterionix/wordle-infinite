1e21f1dc40af66e0b3113c69cac63e25cda8c916
Xterionix <xterionyt@gmail.com>
2026-05-31 12:37:04 -0500

    - Remove empty use of state in Key component

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -13,7 +13,6 @@ interface Props {
 
 export function Key({ char, children, style, gameState, onClick }: Props) {
 
-    const [hasBeenRevealed, setRevealed] = useState(false);
     const [isFlipping, setFlipping] = useState(false);
     const [nextState, setNextState] = useState(TileState.Unguessed)
     const [state, setState] = useState(TileState.Unguessed)
@@ -24,7 +23,7 @@ export function Key({ char, children, style, gameState, onClick }: Props) {
     }, [gameState.isFlipping])
 
     return (
-        <div title={`${hasBeenRevealed}`} className={`key ion-display-flex ion-align-items-center ion-justify-content-center ${char.length == 1 ? state : 'unguessed'}`} style={style} onClick={() => onClick(char)}>
+        <div className={`key ion-display-flex ion-align-items-center ion-justify-content-center ${char.length == 1 ? state : 'unguessed'}`} style={style} onClick={() => onClick(char)}>
             {children ?? char}
         </div>
     );
