c97d61423e7be717d145027c7e36ca8fba5cefa2
Xterionix <xterionyt@gmail.com>
2026-05-31 18:42:46 -0500

    - Apply theme based on setting

--- src/App.tsx
+++ src/App.tsx
@@ -32,37 +32,50 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+// import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
 import { Route } from 'react-router';
 import Settings from './pages/Settings';
-import PreferenceProvider from './components/PreferenceProvider';
+import PreferenceProvider, { usePreferences } from './components/PreferenceProvider';
 import Statistics from './pages/Statistics';
+import { Theme } from './lib/preferences';
+import { useEffect } from 'react';
 
 setupIonicReact();
 
+const AppContent: React.FC = () => {
+  const preferences = usePreferences();
+  let theme = preferences?.settings.theme == Theme.light ? 'light' : 'dark';
+
+  useEffect(() => {
+    theme = preferences?.settings.theme == Theme.light ? 'light' : 'dark';
+  }, [preferences?.stats])
+
+  return (<IonApp className={theme}>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path={"/"}>
+            <Home />
+          </Route>
+          <Route exact path={"/settings"}>
+            <Settings />
+          </Route>
+          <Route exact path={"/statistics"}>
+            <Statistics />
+          </Route>
+        </IonRouterOutlet>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>)
+};
+
 const App: React.FC = () => (
-  <IonApp>
-    <PreferenceProvider>
-      <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route exact path={"/"}>
-              <Home />
-            </Route>
-            <Route exact path={"/settings"}>
-              <Settings />
-            </Route>
-            <Route exact path={"/statistics"}>
-              <Statistics />
-            </Route>
-          </IonRouterOutlet>
-        </IonTabs>
-      </IonReactRouter>
-    </PreferenceProvider>
-  </IonApp>
-);
+  <PreferenceProvider>
+    <AppContent />
+  </PreferenceProvider>
+)
 
 export default App;
